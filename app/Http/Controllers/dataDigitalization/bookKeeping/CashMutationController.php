c4a26b70c14b59b66147631b58537a0693c0c0e6
zharsuke <alazharisqi@gmail.com>
2024-05-02 21:35:16 +0700

    feat: add cash mutation data

--- app/Http/Controllers/dataDigitalization/bookKeeping/CashMutationController.php
+++ app/Http/Controllers/dataDigitalization/bookKeeping/CashMutationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\dataDigitalization\bookKeeping;
 
 use App\Http\Controllers\Controller;
+use App\Models\AccountModel;
 use App\Models\CashMutationModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -27,7 +28,9 @@ class CashMutationController extends Controller
      */
     public function create()
     {
-        //
+        $cash_mutation_code = "CM-" . date('Ymd') . "-" . str_pad(CashMutationModel::count() + 1, 4, '0', STR_PAD_LEFT);
+        $account = AccountModel::all();
+        return Auth::check() ? view('data-digitalization.book-keeping.cash-mutation.create', ['account' => $account, 'cash_mutation_code' => $cash_mutation_code]) : redirect('/login');
     }
 
     /**
@@ -35,7 +38,33 @@ class CashMutationController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'cash_mutation_code' => 'required',
+            'account_debit_id' => 'required',
+            'debit' => 'required',
+            'account_credit_id' => 'required',
+            'credit' => 'required',
+            'description' => 'required',
+        ]);
+
+        AccountModel::find($request->account_debit_id)->update([
+            'balance' => AccountModel::find($request->account_debit_id)->balance + $request->debit
+        ]);
+
+        AccountModel::find($request->account_credit_id)->update([
+            'balance' => AccountModel::find($request->account_credit_id)->balance - $request->credit
+        ]);
+
+        CashMutationModel::create([
+            'cash_mutation_code' => $request->cash_mutation_code,
+            'account_debit_id' => $request->account_debit_id,
+            'debit' => $request->debit,
+            'account_credit_id' => $request->account_credit_id,
+            'credit' => $request->credit,
+            'description' => $request->description,
+            'is_archived' => false
+        ]);
+        return redirect('/data/bookkeeping/cash')->with('success', 'Cash Mutation has been added');
     }
 
     /**
@@ -45,29 +74,5 @@ class CashMutationController extends Controller
     {
         $cashMutation = CashMutationModel::find($id);
         return Auth::check() ? view('data-digitalization.book-keeping.cash-mutation.show', compact('cashMutation')) : redirect('/login');
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
     }
 }
---
+++ resources/views/data-digitalization/book-keeping/cash-mutation/create.blade.php
@@ -0,0 +1,74 @@
+@extends('admin-layout.base')
+
+@section('content')
+    @vite('resources/css/resident/app.css')
+    <div class="py-12 bg-white shadow-xl rounded-t-lg mx-5 mt-5">
+        <div class="max-w-7-xl mx-auto sm:px-6 lg:px-8">
+            <h3 class="mb-3 text-3xl">Cash Mutation</h3>
+            <div class="flex w-full justify-between">
+                <div class=" breadcrumbs mb-3">
+                    <ul>
+                        <li><a href="{{ url('data') }}">Home</a></li>
+                        <li><a href="{{ url('data/bookkeeping/cash') }}">Cash Mutation</a></li>
+                        <li>Add Cash Mutation</li>
+                    </ul>
+                </div>
+            </div>
+            {{-- content --}}
+            <form action="{{ url('data/bookkeeping/cash') }}" method="POST" class="flex flex-col space-y-4 w-full form mr-3">
+                @csrf
+                @if ($errors->any())
+                    <div class="bg-red-100 text-red-500 p-2 rounded-lg">
+                        <ul class="list-disc list-inside">
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+                <div class="flex flex-col space-y-1">
+                    <label for="cash_mutation_code">Cash Mutation Code</label>
+                    <input type="text" name="cash_mutation_code" id="cash_mutation_code" class="rounded-md border border-gray-300 p-2"
+                        placeholder="Enter your name account" value="{{ $cash_mutation_code }}" readonly>
+                </div>
+                <div class="flex flex-col space-y-1">
+                    <label for="account_credit_id">Account Credit</label>
+                    <select name="account_credit_id" id="account_credit_id" class="rounded-md border border-gray-300 p-2">
+                        <option disabled selected value="">- Choose Cash Account -</option>
+                            @foreach ($account as $item)
+                            <option value="{{ $item->account_id }}">{{ $item->name_account }}</option>
+                            @endforeach
+                    </select>
+                </div>
+                <div class="flex flex-col space-y-1">
+                    <label for="credit">Credit</label>
+                    <input type="text" name="credit" id="credit" class="rounded-md border border-gray-300 p-2"
+                        placeholder="Enter your amount credit">
+                </div>
+                <div class="flex flex-col space-y-1">
+                    <label for="account_debit_id">Account Debit</label>
+                    <select name="account_debit_id" id="account_debit_id" class="rounded-md border border-gray-300 p-2">
+                        <option disabled selected value="">- Choose Cash Account -</option>
+                            @foreach ($account as $item)
+                            <option value="{{ $item->account_id }}">{{ $item->name_account }}</option>
+                            @endforeach
+                    </select>
+                </div>
+                <div class="flex flex-col space-y-1">
+                    <label for="debit">Debit</label>
+                    <input type="text" name="debit" id="debit" class="rounded-md border border-gray-300 p-2"
+                        placeholder="Enter your amount debit">
+                </div>
+                <div class="flex flex-col space-y-1">
+                    <label for="description">Description</label>
+                    <input type="text" name="description" id="description" class="rounded-md border border-gray-300 p-2"
+                        placeholder="Enter your description">
+                </div>
+                <div class="flex">
+                    <button type="submit" class="add-btn btn-sm px-4 py-1.5 text-white rounded-md mt-5 w-1/6">Add</button>
+                    <a class="add-btn btn-sm px-4 py-1.5 text-white rounded-md mt-5 w-1/6 mx-3 text-center" href="{{ url('data/bookkeeping/cash') }}">Back</a>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
