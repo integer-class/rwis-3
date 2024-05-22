935e5ef00a76ca18c265759858fa00cd6eda7964
zharsuke <alazharisqi@gmail.com>
2024-05-22 16:32:23 +0700

    feat: add update status done issue report

--- resources/views/issue/report/index.blade.php
+++ resources/views/issue/report/index.blade.php
@@ -229,12 +229,59 @@
                 <div class="bg-green-300 p-3 rounded-md">
                     <h3 class="text-xl">Done</h3>
                     @foreach ($done as $item)
+                        @php
+                            $modalId = 'modal_' . $item->issue_report_id;
+                        @endphp
                         <div class="card card-compact w56 bg-base-100 shadow-xl my-3 rounded-md">
                             <div class="card-body">
                                 <h2 class="card-title">{{ $item->title }}</h2>
                                 <p>{{ $item->description }}</p>
                                 <div class="card-actions justify-end">
-                                    <button class="btn btn-primary">Update Status</button>
+                                    <button class="btn btn-primary"
+                                        onclick="document.getElementById('{{ $modalId }}').showModal()">Update
+                                        Status</button>
+                                    {{-- modal --}}
+                                    <dialog id="{{ $modalId }}" class="modal">
+                                        <div class="modal-box">
+                                            <form method="dialog">
+                                                <button
+                                                    class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                                            </form>
+                                            <h3 class="font-bold text-lg mb-3">Update Status</h3>
+                                            <form action="{{ url('issue/report/' . $item->issue_report_id) }}"
+                                                method="POST" class="flex flex-col space-y-4 w-full form mr-3">
+                                                @csrf
+                                                {!! method_field('PUT') !!}
+                                                <div class="flex flex-col space-y-1 mb-3">
+                                                    <label for="title">Title</label>
+                                                    <input type="text" name="title" id="title"
+                                                        class="rounded-md border border-gray-300 p-2"
+                                                        placeholder="Enter your full name" value="{{ $item->title }}"
+                                                        readonly>
+                                                </div>
+                                                <div class="flex flex-col space-y-1 mb-3">
+                                                    <label for="description">Description</label>
+                                                    <textarea class="rounded-md border border-gray-300 p-2" name="description" id="description" cols="20"
+                                                        rows="10" readonly>{{ $item->description }}</textarea>
+                                                </div>
+                                                <div class="flex flex-col space-y-1">
+                                                    <label for="status">Status</label>
+                                                    <select name="status" id="status"
+                                                        class="rounded-md border border-gray-300 p-2">
+                                                        <option disabled selected
+                                                            value="{{ old('status', $item->status) }}">- Status -</option>
+                                                        @foreach ($status as $statusItem)
+                                                            <option value="{{ $statusItem }}">{{ $statusItem }}
+                                                            </option>
+                                                        @endforeach
+                                                    </select>
+                                                </div>
+                                                <div class="flex flex-row justify-end">
+                                                    <button class="btn btn-primary mx-2">Update</button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </dialog>
                                 </div>
                             </div>
                         </div>
