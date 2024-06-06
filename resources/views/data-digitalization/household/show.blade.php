67a899e4bc536d395204fa363fb02610567853e1
zharsuke <alazharisqi@gmail.com>
2024-06-06 14:26:45 +0700

    chore(anu): adjust display show details and format number

--- resources/views/data-digitalization/household/show.blade.php
+++ resources/views/data-digitalization/household/show.blade.php
@@ -21,15 +21,15 @@
                         <dt class="text-sm font-medium text-gray-500">Number KK</dt>
                         <dd class="mt-1 text-sm text-gray-900 sm:col-span-2">{{ $household->number_kk }}</dd>
                     </div>
-                    <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                         <dt class="text-sm font-medium text-gray-500">Family Head</dt>
                         <dd class="mt-1 text-sm text-gray-900 sm:col-span-2">{{ $household->familyHead->full_name }}</dd>
                     </div>
-                    <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                         <dt class="text-sm font-medium text-gray-500">Full Address</dt>
                         <dd class="mt-1 text-sm text-gray-900 sm:col-span-2">{{ $household->full_address }}</dd>
                     </div>
-                    <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                         <dt class="text-sm font-medium text-gray-500">Area</dt>
                         <dd class="mt-1 text-sm text-gray-900 sm:col-span-2">{{ $household->area }}m&sup2</dd>
                     </div>
@@ -49,10 +49,10 @@
                     <tbody>
                         @foreach ($resident as $item)
                         <tr class="hover">
-                                <td>{{ $item->full_name }}</td>
-                                <td>{{ $item->nik }}</td>
-                            </tr>
-                            @endforeach
+                            <td><a href="{{ url('data/resident/show/' . $item->resident_id) }}">{{ $item->full_name }}</a></td>
+                            <td><a href="{{ url('data/resident/show/' . $item->resident_id) }}">{{ $item->nik }}</a></td>
+                        </tr>
+                    @endforeach
                     </tbody>
                 </table>
             </div>
