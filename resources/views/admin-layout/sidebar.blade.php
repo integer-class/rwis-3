e8fceb14568aebf7560ae4fd149a29a8aba8f59f
elianiva <dicha.arkana03@gmail.com>
2024-06-09 06:12:05 +0700

    refactor: more readable sidebar

--- resources/views/admin-layout/sidebar.blade.php
+++ resources/views/admin-layout/sidebar.blade.php
@@ -1,126 +1,72 @@
 <div class="drawer lg:drawer-open">
     <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
     <div class="drawer-side">
-        <a href="{{ url('/') }}">
-            <div class="flex gap-2 justify-center items-center mt-7">
+        <div class="my-8">
+            <a href="{{ url('/') }}" class="flex gap-2 justify-center items-center">
                 <img src="{{ asset('img/icon.png') }}" alt="icon" width="70">
                 <h6 class="font-bold">RW 11<br>Information<br>System</h6>
-            </div>
-        </a>
+            </a>
+        </div>
         <ul class="menu menu-md w-72 rounded-box">
             {{-- Dashboard --}}
-            <li>
-                <a class="dashboard" href="{{ url('dashboard') }}">
-                    <div class="flex">
-                        <span class="icon-[ic--baseline-space-dashboard] text-xl mt-0.5"></span>
-                        <h6 class="mx-4 mt-1">Dashboard</h6>
-                    </div>
-                </a>
-            </li>
+            <x-menu-item href="{{ route('dashboard') }}" icon="ic--baseline-space-dashboard" title="Dashboard" />
             {{-- Information Centre --}}
-            <li>
-                <details id="information">
-                    <summary>
-                        <span class="icon-[ic--outline-info] text-xl mt-0.5"></span>
-                        <h6 class="mx-2 mt-1">Information Centre</h6>
-                    </summary>
-                    <ul>
-                        <li id="facility"><a href="{{ url('information/facility') }}">
-                                <span class="icon-[ic--round-location-city] text-xl"></span>Facility
-                            </a>
-                        </li>
-                        <li id="umkm"><a href="{{ url('information/umkm') }}"><span
-                                    class="icon-[ic--round-store-mall-directory] text-xl"></span>UMKM</a></li>
-                    </ul>
-                </details>
-            </li>
+            <x-menu-item
+                icon="ic--outline-info"
+                title="Information Centre"
+                :submenu="[
+                    ['href' => route('information.facility.index'), 'icon' => 'ic--round-location-city', 'title' => 'Facility'],
+                    ['href' => route('information.umkm.index'), 'icon' => 'ic--round-store-mall-directory', 'title' => 'UMKM'],
+                ]"
+            />
             {{-- Data Digitalization --}}
-            <li>
-                <details id="data">
-                    <summary>
-                        <span class="icon-[ic--baseline-public] text-xl mt-0.5"></span>
-                        <h6 class="mx-2 mt-1">Data Digitalization</h6>
-                    </summary>
-                    <ul>
-                        <li id="household"><a href="{{ url('data/household') }}"><span
-                                    class="icon-[ic--outline-other-houses] text-xl"></span>Household</a></li>
-                        <li id="resident"><a href="{{ url('data/resident') }}"><span
-                                    class="icon-[ic--baseline-person-outline] text-xl"></span>Resident</a></li>
-                        <li id="asset"><a href="{{ url('data/asset') }}"><span
-                                    class="icon-[ic--baseline-attach-money] text-xl"></span>Asset</a></li>
-                        {{-- Book Keeping --}}
-                        <li>
-                            <details id="bookkeeping">
-                                <summary>
-                                    <span class="icon-[ic--outline-bookmark-added] text-xl mt-0.5"></span>
-                                    <h6 class="mx-2 mt-1">Book Keeping</h6>
-                                </summary>
-                                <ul>
-                                    <li id="account"><a href="{{ url('data/bookkeeping/account') }}"><span
-                                                class="icon-[ic--round-account-balance] text-xl"></span>Master
-                                            Account</a></li>
-                                    <li id="cash"><a href="{{ url('data/bookkeeping/cash') }}"><span
-                                                class="icon-[ic--round-attach-money] text-xl"></span>Cash
-                                            Mutation</a>
-                                    </li>
-                                </ul>
-                            </details>
-                        </li>
-                    </ul>
-                </details>
-            </li>
+            <x-menu-item
+                icon="ic--baseline-public"
+                title="Data Digitalization"
+                :submenu="[
+                    ['href' => route('data.household.index'), 'icon' => 'ic--outline-other-houses', 'title' => 'Household'],
+                    ['href' => route('data.resident.index'), 'icon' => 'ic--baseline-person-outline', 'title' => 'Resident'],
+                    ['href' => route('data.asset.index'), 'icon' => 'ic--baseline-attach-money', 'title' => 'Asset'],
+                    [
+                        'href' => '#',
+                        'icon' => 'ic--outline-bookmark-added',
+                        'title' => 'Book Keeping',
+                        'submenu' => [
+                            ['href' => route('data.bookkeeping.account.index'), 'icon' => 'ic--round-account-balance', 'title' => 'Master Account'],
+                            ['href' => route('data.bookkeeping.cash.index'), 'icon' => 'ic--round-attach-money', 'title' => 'Cash Mutation'],
+                        ],
+                    ],
+                ]"
+            />
             {{-- Issue Tracker --}}
-            <li>
-                <details id="issue">
-                    <summary>
-                        <span class="icon-[ic--round-track-changes] text-xl mt-0.5"></span>
-                        <h6 class="mx-2 mt-1">Issue Tracker</h6>
-                    </summary>
-                    <ul>
-                        <li id="approval"><a href="{{ url('issue/approval') }}"><span
-                                    class="icon-[material-symbols--notifications-unread-outline-rounded] text-xl"></span>Approval
-                                Issue</a>
-                        </li>
-                        <li id="report"><a href="{{ url('issue/report') }}"><span
-                                    class="icon-[material-symbols--report-outline-rounded] text-xl"></span>Issue
-                                Report</a></li>
-                    </ul>
-                </details>
-            </li>
+            <x-menu-item
+                icon="ic--round-track-changes"
+                title="Issue Tracker"
+                :submenu="[
+                    ['href' => route('issue.approval.index'), 'icon' => 'material-symbols--notifications-unread-outline-rounded', 'title' => 'Approval Issue'],
+                    ['href' => route('issue.report.index'), 'icon' => 'material-symbols--report-outline-rounded', 'title' => 'Issue Report'],
+                ]"
+            />
             {{-- Broadcast --}}
-            <li>
-                <details id="broadcast">
-                    <summary>
-                        <span class="icon-[ic--round-podcasts] text-xl mt-0.5"></span>
-                        <h6 class="mx-2 mt-1">Broadcast</h6>
-                    </summary>
-                    <ul>
-                        <li id="template"><a href="{{ url('broadcast/template') }}"><span
-                                    class="icon-[material-symbols--chat-outline-rounded] text-xl"></span>Template</a>
-                        </li>
-                        <li id="schedule"><a href="{{ url('broadcast/scheduled') }}"><span
-                                    class="icon-[material-symbols--nest-clock-farsight-analog-outline-rounded] text-xl"></span>Scheduled</a>
-                        </li>
-                    </ul>
-                </details>
-            </li>
+            <x-menu-item
+                icon="ic--round-podcasts"
+                title="Broadcast"
+                :submenu="[
+                    ['href' => route('broadcast.template.index'), 'icon' => 'material-symbols--chat-outline-rounded', 'title' => 'Template'],
+                    ['href' => route('broadcast.scheduled.index'), 'icon' => 'material-symbols--nest-clock-farsight-analog-outline-rounded', 'title' => 'Scheduled'],
+                ]"
+            />
             {{-- social-aid aid --}}
-            <li>
-                <details id="social">
-                    <summary>
-                        <span class="icon-[ic--round-attach-money] text-xl mt-0.5"></span>
-                        <h6 class="mx-2 mt-1">Social Aid</h6>
-                    </summary>
-                    <ul>
-                        <li id="calculate"><a href="{{ url('social-aid/calculate') }}"><span
-                                    class="icon-[material-symbols--calculate] text-xl"></span>Calculate</a>
-                        </li>
-                    </ul>
-                </details>
-            </li>
+            <x-menu-item
+                icon="ic--round-attach-money"
+                title="Social Aid"
+                :submenu="[
+                    ['href' => route('social-aid.calculate.index'), 'icon' => 'material-symbols--calculate', 'title' => 'Calculate'],
+                ]"
+            />
             {{-- Logout --}}
             <div tabindex="0" class="mt-5 text-center">
-                <form action="{{ url('/logout') }}" method="POST">
+                <form action="{{ route('auth.logout') }}" method="POST">
                     @csrf
                     <button class="add-btn btn-sm w-5/6 text-white rounded-md" type="submit">Logout</button>
                 </form>
@@ -129,137 +75,9 @@
     </div>
 </div>
 
-<script>
-    const currentUrl = window.location.href;
-
-    if (currentUrl.includes('/information')) {
-        const menu = document.getElementById('information');
-        if (menu) {
-            menu.setAttribute('open', 'true');
-
-            if (currentUrl.includes('/information/facility')) {
-                const subMenu = document.getElementById('facility');
-                if (subMenu) {
-                    subMenu.setAttribute('class', 'add-btn text-white rounded-md');
-                }
-            }
-
-            if (currentUrl.includes('/information/umkm')) {
-                const subMenu = document.getElementById('umkm');
-                if (subMenu) {
-                    subMenu.setAttribute('class', 'add-btn text-white rounded-md');
-                }
-            }
-        }
-    }
-
-    if (currentUrl.includes('/data')) {
-        const menu = document.getElementById('data');
-        if (menu) {
-            menu.setAttribute('open', 'true');
-
-            if (currentUrl.includes('/data/resident')) {
-                const subMenu = document.getElementById('resident');
-                if (subMenu) {
-                    subMenu.setAttribute('class', 'add-btn text-white rounded-md');
-                }
-            }
-
-            if (currentUrl.includes('/data/household')) {
-                const subMenu = document.getElementById('household');
-                if (subMenu) {
-                    subMenu.setAttribute('class', 'add-btn text-white rounded-md');
-                }
-            }
-
-            if (currentUrl.includes('/data/asset')) {
-                const subMenu = document.getElementById('asset');
-                if (subMenu) {
-                    subMenu.setAttribute('class', 'add-btn text-white rounded-md');
-                }
-            }
-
-            if (currentUrl.includes('/data/bookkeeping')) {
-                const subMenu = document.getElementById('bookkeeping');
-                if (subMenu) {
-                    subMenu.setAttribute('open', 'true');
-                    if (currentUrl.includes('/data/bookkeeping/account')) {
-                        const subSubMenu = document.getElementById('account');
-                        if (subSubMenu) {
-                            subSubMenu.setAttribute('class', 'add-btn text-white rounded-md');
-                        }
-                    }
-                    if (currentUrl.includes('/data/bookkeeping/cash')) {
-                        const subSubMenu = document.getElementById('cash');
-                        if (subSubMenu) {
-                            subSubMenu.setAttribute('class', 'add-btn text-white rounded-md');
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    if (currentUrl.includes('/issue')) {
-        const menu = document.getElementById('issue');
-        if (menu) {
-            menu.setAttribute('open', 'true');
-
-            if (currentUrl.includes('/issue/approval')) {
-                const subMenu = document.getElementById('approval');
-                if (subMenu) {
-                    subMenu.setAttribute('class', 'add-btn text-white rounded-md');
-                }
-            }
-
-            if (currentUrl.includes('/issue/report')) {
-                const subMenu = document.getElementById('report');
-                if (subMenu) {
-                    subMenu.setAttribute('class', 'add-btn text-white rounded-md');
-                }
-            }
-        }
-    }
-
-    if (currentUrl.includes('/broadcast')) {
-        const menu = document.getElementById('broadcast');
-        if (menu) {
-            menu.setAttribute('open', 'true');
-
-            if (currentUrl.includes('/broadcast/template')) {
-                const subMenu = document.getElementById('template');
-                if (subMenu) {
-                    subMenu.setAttribute('class', 'add-btn text-white rounded-md');
-                }
-            }
-
-            if (currentUrl.includes('/broadcast/schedule')) {
-                const subMenu = document.getElementById('schedule');
-                if (subMenu) {
-                    subMenu.setAttribute('class', 'add-btn text-white rounded-md');
-                }
-            }
-        }
-    }
-
-    if (currentUrl.includes('/social-aid')) {
-        const menu = document.getElementById('social-aid');
-        if (menu) {
-            menu.setAttribute('open', 'true');
-
-            if (currentUrl.includes('/social-aid/calculate')) {
-                const subMenu = document.getElementById('calculate');
-                if (subMenu) {
-                    subMenu.setAttribute('class', 'add-btn text-white rounded-md');
-                }
-            }
-
-            if (currentUrl.includes('/social-aid/schedule')) {
-                const subMenu = document.getElementById('schedule');
-                if (subMenu) {
-                    subMenu.setAttribute('class', 'add-btn text-white rounded-md');
-                }
-            }
-        }
-    }
-</script>
+@push('body_scripts')
+    <script>
+        const currentUrl = window.location.href;
+        @stack('sidebar_script')
+    </script>
+@endpush
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,56 @@
 <!DOCTYPE html>
 <html lang="en" data-theme="light">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        @vite('resources/css/app.css')
+        <title>RWIS | Login</title>
+    </head>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite('resources/css/app.css')
-    @vite('resources/css/auth/app.css')
-    <title>RWIS | Login</title>
-</head>
-
-<body class="flex h-screen items-center justify-center">
-    <main>
-        <div class="login-container rounded-lg shadow-2xl flex w-full">
-
-            {{-- image --}}
-            <div class="w-2/3">
-                <img class="rounded-l-lg" src="{{ asset('img/bg-login.png') }}" width="500" alt="">
-            </div>
-
-            {{-- form --}}
-            <div class="w-1/2 flex flex-col justify-center items-center space-y-5">
-                <div class="flex gap-4">
-                    <img src="{{ asset('img/icon.png') }}" width="94" alt="">
-                    <h1 class="text-2xl font-bold">RW 11<br>Information<br>System</h1>
+    <body class="flex h-screen items-center justify-center">
+        <main>
+            <div class="rounded-lg max-w-screen-lg shadow-2xl flex w-full">
+                {{-- image --}}
+                <div class="w-2/3">
+                    <img class="rounded-l-lg" src="{{ asset('img/bg-login.png') }}" width="500" alt="">
                 </div>
-                <form action="{{ url('/auth') }}" method="POST" class="flex flex-col space-y-4 w-full form mr-3">
-                    @csrf
-                    @if ($errors->any())
-                        <div class="bg-red-100 text-red-500 p-2 rounded-lg">
-                            <ul class="list-disc list-inside">
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
+                {{-- form --}}
+                <div class="w-1/2 flex flex-col justify-center items-center space-y-5">
+                    <div class="flex gap-4">
+                        <img src="{{ asset('img/icon.png') }}" width="94" alt="">
+                        <h1 class="text-2xl font-bold">RW 11<br>Information<br>System</h1>
+                    </div>
+                    <form action="{{ url('/auth') }}" method="POST" class="flex flex-col space-y-4 w-full px-8">
+                        @csrf
+                        @if ($errors->any())
+                            <div class="bg-red-100 text-red-500 p-2 rounded-lg">
+                                <ul class="list-disc list-inside">
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+                        <div class="flex flex-col space-y-1">
+                            <label for="username">Username</label>
+                            <input
+                                type="text" name="name" id="username" class="rounded-md border border-gray-300 p-2"
+                                placeholder="Username" autofocus
+                            >
                         </div>
-                    @endif
-                    <div class="flex flex-col space-y-1">
-                        <label for="username">Username</label>
-                        <input type="text" name="name" id="username"
-                            class="rounded-md border border-gray-300 p-2" placeholder="Username" autofocus>
-                    </div>
-                    <div class="flex flex-col space-y-1">
-                        <label for="password">Password</label>
-                        <input type="password" name="password" id="password"
-                            class="rounded-md border border-gray-300 p-2" placeholder="Password">
-                    </div>
-                    <button type="submit" class="login-btn text-white rounded-lg p-2">Login</button>
-                </form>
+                        <div class="flex flex-col space-y-1">
+                            <label for="password">Password</label>
+                            <input
+                                type="password" name="password" id="password"
+                                class="rounded-md border border-gray-300 p-2" placeholder="Password"
+                            >
+                        </div>
+                        <button type="submit" class="btn btn-primary text-white rounded-lg">Login</button>
+                    </form>
+                </div>
             </div>
-        </div>
-    </main>
-</body>
+        </main>
+    </body>
 
 </html>
