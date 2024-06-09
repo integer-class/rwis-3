e66aa9d2e0931e008485c3230298c20a444f5281
zharsuke <alazharisqi@gmail.com>
2024-06-09 22:44:19 +0700

    fix(financial-contribution): Call to a member function pluck() on array

--- resources/views/user-layout/financial.blade.php
+++ resources/views/user-layout/financial.blade.php
@@ -11,49 +11,81 @@
 </head>
 
 <body class="h-full font-rubik">
-<div class="bg-base-100 h-full">
-    @include('user-layout.header')
-    <div id="content" class="py-44">
-        <h2 class="text-4xl font-bold mb-16 text-center text-slate-800">Financial Contribution</h2>
-        <div class="grid grid-cols-3 gap-4 px-4 max-w-screen-lg mx-auto">
-            @foreach ($mutations as $df)
-                @push('body_scripts')
-                <script>
-                    const mutationCtx = document.getElementById('mutation');
-                    new Chart(mutationCtx, {
-                        type: 'line',
-                        data: {
-                            labels: @json($df->pluck('date')),
-                            datasets: [{
-                                label: 'Mutasi Iuran',
-                                data: @json($df->pluck('total')),
-                                borderWidth: 1,
-                                backgroundColor: [
-                                    'rgba(75, 192, 192, 0.2)',
-                                ],
-                                borderColor: [
-                                    'rgb(75, 192, 192)',
-                                ],
-                            }]
+    <div class="bg-base-100 h-full">
+        @include('user-layout.header')
+        <div id="content" class="py-44">
+            <h2 class="text-4xl font-bold mb-16 text-center text-slate-800">Financial Contribution</h2>
+            <div class="grid grid-cols-3 gap-4 px-4 max-w-screen-lg mx-auto">
+                <div class="col-span-3 h-full border p-4 rounded-md">
+                    <canvas id="mutation" width="360" height="130"></canvas>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    @push('head_scripts')
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    @endpush
+
+    @push('body_scripts')
+    <script>
+        document.addEventListener('DOMContentLoaded', (event) => {
+            const mutationCtx = document.getElementById('mutation').getContext('2d');
+            new Chart(mutationCtx, {
+                type: 'line',
+                data: {
+                    labels: @json($mutations->pluck('date')),
+                    datasets: [{
+                        label: 'Mutasi Iuran',
+                        data: @json($mutations->pluck('total')),
+                        borderWidth: 1,
+                        backgroundColor: [
+                            'rgba(75, 192, 192, 0.2)',
+                        ],
+                        borderColor: [
+                            'rgb(75, 192, 192)',
+                        ],
+                    }]
+                },
+                options: {
+                    plugins: {
+                        legend: {
+                            labels: {
+                                font: {
+                                    size: 16
+                                }
+                            }
                         },
-                        options: {
-                            scales: {
-                                y: {
-                                    grid: {
-                                        display: true
-                                    }
-                                },
-                                x: {
-                                    grid: {
-                                        display: false
-                                    }
+                    },
+                    scales: {
+                        y: {
+                            grid: {
+                                display: true
+                            },
+                            ticks: {
+                                font: {
+                                    size: 14
+                                }
+                            }
+                        },
+                        x: {
+                            grid: {
+                                display: false
+                            },
+                            ticks: {
+                                font: {
+                                    size: 14
                                 }
                             }
                         }
-                    });
-                </script>
-                @endpush
-            @endforeach
-        </div>
-    </div>
-</div>
+                    }
+                }
+            });
+        });
+    </script>
+    @endpush
+
+    @stack('head_scripts')
+    @stack('body_scripts')
+</body>
+</html>
